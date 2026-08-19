26925b80dc4f8bf005ecf90c695c37988a3268a3
Muhammad Farzad Ali <farzad0514@gmail.com>
2026-08-19 18:14:05 +0200

    add update

--- src/lib/components/SparqlCodeBlock.svelte
+++ src/lib/components/SparqlCodeBlock.svelte
@@ -10,18 +10,14 @@
     isEditing = true;
   }
 
-  function cancelEdit() {
-    isEditing = false;
+  function revertEdit() {
     editedCode = code;
-  }
-
-  function confirmEdit() {
     isEditing = false;
   }
 
   function handleKeydown(event) {
     if (event.key === 'Escape') {
-      cancelEdit();
+      revertEdit();
     }
   }
 
@@ -40,6 +36,7 @@
   function handleExecute() {
     if (onExecute) {
       onExecute(isEditing ? editedCode : code);
+      isEditing = false;
     }
   }
 </script>
@@ -62,15 +59,10 @@
       </button>
 
       {#if isEditing}
-        <button class="icon-btn" onclick={confirmEdit} title="Confirm edit">
+        <button class="icon-btn" onclick={revertEdit} title="Revert changes">
           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M20 6 9 17l-5-5"></path>
-          </svg>
-        </button>
-        <button class="icon-btn" onclick={cancelEdit} title="Cancel edit">
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M18 6 6 18"></path>
-            <path d="m6 6 12 12"></path>
+            <path d="M3 7v6h6"></path>
+            <path d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13"></path>
           </svg>
         </button>
       {:else}
